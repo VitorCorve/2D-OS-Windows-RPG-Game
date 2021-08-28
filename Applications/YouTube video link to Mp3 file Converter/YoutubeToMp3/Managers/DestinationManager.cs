b0d945d72a6aff500fc82e09dcc4c71c9fb2a11c
VitorCorve <cosm.realty@gmail.com>
2021-08-28 16:24:28 +0300

    Youtube like to Mp3 app restructurizaton

---
+++ Applications/YouTube video link to Mp3 file Converter/YoutubeToMp3/Managers/DestinationManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace YoutubeToMp3.Managers
+{
+    public static class DestinationManager
+    {
+        public static string DestinationPath { get; set; }
+        public static void CheckActualDestinationPath()
+        {
+            string destinationPath = $"{Environment.CurrentDirectory}" + "\\Destination.txt";
+            if (File.Exists(destinationPath))
+                DestinationPath = File.ReadAllText(destinationPath);
+            else
+                File.WriteAllText($"{Environment.CurrentDirectory}" + "\\Destination.txt", "E:\\Mus");
+        }
+        public static void UpdateDestinationPath(string path) => File.WriteAllText($"{Environment.CurrentDirectory}" + "\\Destination.txt", path);
+    }
+}
